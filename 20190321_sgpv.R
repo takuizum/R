e8fdcde0dc072cde4e10aacebe23cd84220bf106
takuizum <sep10.taku.izum@gmail.com>
2019-06-04 23:38:24 +0900

    upload sgpv.R(made before)

--- 20190321_sgpv.R
+++ 20190321_sgpv.R
@@ -23,3 +23,19 @@ t_test_res$conf.int[1:2]
 plot(extra ~ group, data = sleep)
 with(sleep, t.test(extra[group == 1], extra[group == 2]))
 t.test(extra ~ group, data = sleep)
+
+
+# sgpv plot
+data(leukstats)
+plotsgpv(est.lo=leukstats$ci.lo, est.hi=leukstats$ci.hi,
+         null.lo=-0.3, null.hi=0.3,
+         set.order=order(leukstats$p.value),
+         x.show=7000,
+         plot.axis=c("TRUE","FALSE"),
+         null.pt=0, outline.zone=TRUE,
+         title.lab="Leukemia Example", y.lab="Fold Change (base 10)",
+         x.lab="Classical p-value ranking",
+         legend.on=TRUE)
+axis(side=2,at=round(log(c(1/1000,1/100,1/10,1/2,1,2,10,100,1000),
+                         base=10),2),labels=c("1/1000","1/100","1/10","1/2",1,2,10,100,1000),
+     las=2)
